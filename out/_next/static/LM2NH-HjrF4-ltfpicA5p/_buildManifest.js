02ca97ae716cf6f8b70c697f3fe98833a768bb98
yezy <yezileliilomo@hotmail.com>
2021-08-13 17:35:55 +0200

    Deploy Next.js to gh-pages

---
+++ out/_next/static/LM2NH-HjrF4-ltfpicA5p/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-fce5e0cba3ae5a7fde94.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
